26da4777caee6b28904cf9a29678f7a82e974964
Michael Craun <michael.craun@gmail.com>
2021-10-14 07:50:13 -0400

    Initial Commit

---
+++ Shared/you_are_specialApp.swift
@@ -0,0 +1,17 @@
+//
+//  you_are_specialApp.swift
+//  Shared
+//
+//  Created by Michael Craun on 10/14/21.
+//
+
+import SwiftUI
+
+@main
+struct you_are_specialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
